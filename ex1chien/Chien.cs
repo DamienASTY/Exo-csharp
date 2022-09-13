072d9d62a0b77830142a5524ad887ec7e8905a38
DamienASTY <200051@site.asty-moulin.be>
2022-09-13 14:24:09 +0200

    Commit final

--- ex1chien/Chien.cs
+++ ex1chien/Chien.cs
@@ -4,15 +4,29 @@ public class Chien
 {
     private string nom;
     private string race;
+    private int age;
+    private static int instance;
     
-    public Chien(string nom, string race)
+    public Chien(string nom, string race, int age)
     {
         this.nom = nom;
         this.race = race;
+        this.age = age;
+        instance++;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public static int getIstance()
+    {
+        return instance;
     }
 
     public virtual string? ToString()
     {
-        return "Nom : " + this.nom + " race : " + this.race;
+        return "Nom : " + this.nom + " race : " + this.race + " age : " + age + " ans";
     }
 }
--- ex1chien/Program.cs
+++ ex1chien/Program.cs
@@ -7,7 +7,14 @@ namespace Porgram
         static void Main(string[] arg)
         {
             Console.WriteLine("Hello world");
-            Chien monChien = new Chien("Medor", "Waf");
+            Chien monChien = new Chien("Medor", "Waf", 4);
+            Chien monChien2 = new Chien("Fido", "Chiuaua", 5);
+            Chien mchien = new Chien("Pupuce", "Yorkshire", 13);
+
+            int age = monChien.getAge() + monChien2.getAge() + mchien.getAge();
+            float moyenne = age / Chien.getIstance();
+            
+            Console.WriteLine("Age moyenne des chiens : " + moyenne + " ans");
             Console.WriteLine(monChien.ToString());
         }
     }
