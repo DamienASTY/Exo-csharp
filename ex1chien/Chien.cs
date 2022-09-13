05e1579a1342595062b45072e5b08f909825b24b
DamienASTY <200051@site.asty-moulin.be>
2022-09-13 14:11:03 +0200

    Ajout des fichiers principaux

---
+++ ex1chien/Chien.cs
@@ -0,0 +1,18 @@
+﻿namespace Porgram;
+
+public class Chien
+{
+    private string nom;
+    private string race;
+    
+    public Chien(string nom, string race)
+    {
+        this.nom = nom;
+        this.race = race;
+    }
+
+    public virtual string? ToString()
+    {
+        return "Nom : " + this.nom + " race : " + this.race;
+    }
+}
---
+++ ex1chien/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Porgram
+{
+    internal class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Hello world");
+            Chien monChien = new Chien("Medor", "Waf");
+            Console.WriteLine(monChien.ToString());
+        }
+    }
+}
